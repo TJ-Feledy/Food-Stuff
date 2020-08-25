4bb43d3ecd8610f4a76872c4474e5eb123e65c5d
TJ-Feledy <teej387@yahoo.com>
2020-08-25 18:34:47 -0500

    Added intro to landing page

--- food-stuff/src/Components/LandingPage/LandingPage.js
+++ food-stuff/src/Components/LandingPage/LandingPage.js
@@ -4,7 +4,7 @@ function LandingPage(props) {
     return (
         <div className='LandingPage'>
             <h1 className='welcome'>Welcome to <span className='food-stuff'>FOOD-STUFF</span>!</h1>
-
+            <span className='intro'>A place to conveniently find, price, and list all of your food stuff.</span>
         </div>
     )
 }
