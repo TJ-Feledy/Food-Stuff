4cf6372440e592c242a07eb00370b64cf735b2f6
TJ-Feledy <teej387@yahoo.com>
2020-08-25 22:11:15 -0500

    adjusted size and color of logo and intro

--- food-stuff/src/Components/LandingPage/LandingPage.js
+++ food-stuff/src/Components/LandingPage/LandingPage.js
@@ -8,8 +8,8 @@ function LandingPage(props) {
     console.log(iconList(iconSet))
     return (
         <div className='LandingPage'>
-            <div className='fsIconContainer'><IcomoonReact className='fsIcon' iconSet={iconSet} size={150} icon='food-stuff'/></div>
-            <span className='intro'>A place to conveniently find, price, and list all of your food stuff.</span>
+            <div className='fsIconContainer'><IcomoonReact className='fsIcon' iconSet={iconSet} size={150} color='darkred' icon='food-stuff'/></div>
+            <h3 className='intro'>A place to conveniently find, price, and list all of your food stuff.</h3>
         </div>
     )
 }
