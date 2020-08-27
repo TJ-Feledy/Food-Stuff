2831ece8525343b639609c39d10293b8150101f6
TJ-Feledy <teej387@yahoo.com>
2020-08-26 20:34:18 -0500

    added landing nav

--- food-stuff/src/Components/LandingPage/LandingPage.js
+++ food-stuff/src/Components/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import iconSet from "../../Icons/food-stuff-icon/selection.json";
 import IcomoonReact, { iconList } from "icomoon-react";
+import { Link } from 'react-router-dom'
 
 import './LandingPage.css'
 
@@ -10,6 +11,13 @@ function LandingPage(props) {
         <div className='LandingPage'>
             <h3 className='intro'>A place to conveniently create a list of, find, and price all of your food stuff.</h3>
             <div className='fsIconContainer'><IcomoonReact className='fsIcon' iconSet={iconSet} size={125} color='darkred' icon='food-stuff'/></div>
+            <div className='landingNavContainer'>
+                <h5 className='joinUs'>It's time to make a grocery list like a boss!</h5>
+                <div className='landingLinksContainer'>
+                    <Link className='registerLink' to='#'>Register</Link>
+                    <Link className='loginLink' to='#'>Login</Link>
+                </div>
+            </div>
         </div>
     )
 }
