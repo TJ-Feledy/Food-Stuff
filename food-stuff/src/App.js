b98496146c4c7b7a233a7edc781fa0a40c381cab
TJ-Feledy <teej387@yahoo.com>
2020-08-27 23:11:57 -0500

    Imported Register in app and set route

--- food-stuff/src/App.js
+++ food-stuff/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom'
 
 import LandingPage from './Components/LandingPage/LandingPage.js'
+import Register from './Components/Register/Register.js'
 
 function App({location}, props) {
   console.log(location, ' props ', props)
@@ -11,6 +12,7 @@ function App({location}, props) {
       <section className='routeSection'>
         <Switch location={location}>
           <Route path='/' render={props => <LandingPage {...props} />}/>
+          <Route path='/register' render={props => <Register {...props} />}/>
         </Switch>
       </section>
     </div>
