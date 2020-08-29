a72cf1b8bc520a47fd4a0f4b7e6f35c4e7bbe52b
TJ-Feledy <teej387@yahoo.com>
2020-08-28 23:10:08 -0500

    created welcome and intro for register component

--- food-stuff/src/Components/Register/Register.js
+++ food-stuff/src/Components/Register/Register.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 function Register(props) {
-
+    return (
+        <div className='Register'>
+            <h4 className='welcome'>Welcome to Food-Stuff!</h4>
+            <h5 className='registerIntro'>Please create a username and password to get started.</h5>
+        </div>
+    )
 }
 
 export default Register
