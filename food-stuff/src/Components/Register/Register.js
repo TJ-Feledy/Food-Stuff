a258802c06f6763f57b546d4084a31b2bacecf43
TJ-Feledy <teej387@yahoo.com>
2020-08-30 21:57:28 -0500

    Converted Register component to Class componenet

--- food-stuff/src/Components/Register/Register.js
+++ food-stuff/src/Components/Register/Register.js
@@ -1,18 +1,28 @@
 import React from 'react'
 
-function Register(props) {
-    return (
-        <div className='Register'>
-            <h4 className='welcome'>Welcome to Food-Stuff!</h4>
-            <h5 className='registerIntro'>Please create a username and password to get started.</h5>
-            <form className='registerForm'>
-                <label to='username'>Username</label>
-                <input type='text' id='username' name='username' required />
-                <label to='password'>Password</label>
-                <input type='text' id='password' name='password' required />
-            </form>
-        </div>
-    )
+class Register extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    render() {
+        return (
+            <div className='Register'>
+                <h4 className='welcome'>Welcome to Food-Stuff!</h4>
+                <h5 className='registerIntro'>Please create a username and password to get started.</h5>
+                <form className='registerForm'>
+                    <label to='username'>Username</label>
+                    <input type='text' id='username' name='username' required />
+                    <label to='password'>Password</label>
+                    <input type='text' id='password' name='password' required />
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Register
