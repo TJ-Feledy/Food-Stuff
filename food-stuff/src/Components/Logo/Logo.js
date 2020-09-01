b5b4c5d27038e7513058fbbbf7b2121295edeb39
TJ-Feledy <teej387@yahoo.com>
2020-08-31 20:14:46 -0500

    finished Logo component

--- food-stuff/src/Components/Logo/Logo.js
+++ food-stuff/src/Components/Logo/Logo.js
@@ -1,4 +1,13 @@
 import React from 'react'
+import iconSet from "../../Icons/food-stuff-icon/selection.json";
+import IcomoonReact, { iconList } from "icomoon-react";
 
 import './Logo.css'
 
+function Logo() {
+    return (
+        <div className='fsIconContainer'><IcomoonReact className='fsIcon' iconSet={iconSet} size={125} color='darkred' icon='food-stuff'/></div>
+    )
+}
+
+export default Logo
