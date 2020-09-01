5ec80aff1f011a80a7792580360b4c8aa2c38c19
TJ-Feledy <teej387@yahoo.com>
2020-08-31 22:57:48 -0500

    added login link to nav

--- food-stuff/src/Components/Nav/Nav.js
+++ food-stuff/src/Components/Nav/Nav.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import Logo from '../Logo/Logo.js'
 
 import './Nav.css'
@@ -7,6 +9,7 @@ function Nav() {
     return (
         <div className='Nav'>
             <Logo />
+            {window.location.pathname === '/register' ? <Link className='loginLink' to='#'>Login</Link> : <>menu</>}
         </div>
     )
 }
