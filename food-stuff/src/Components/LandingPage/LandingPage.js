bd1c81e969b99c172416af2cdebeb11d962f9561
TJ-Feledy <teej387@yahoo.com>
2020-09-01 20:25:06 -0500

    Reworded intro

--- food-stuff/src/Components/LandingPage/LandingPage.js
+++ food-stuff/src/Components/LandingPage/LandingPage.js
@@ -9,7 +9,7 @@ function LandingPage(props) {
     console.log(iconList(iconSet))
     return (
         <div className='LandingPage'>
-            <h3 className='intro'>A place to conveniently create a list of, find, and price all of your food stuff.</h3>
+            <h3 className='intro'>A place to conveniently find, organize, and create a list of all of your food stuff.</h3>
             <div className='fsIconContainer'><IcomoonReact className='fsIcon' iconSet={iconSet} size={125} color='darkred' icon='food-stuff'/></div>
             <div className='landingNavContainer'>
                 <h4 className='joinUs'>It's time to make a grocery list like a boss!</h4>
