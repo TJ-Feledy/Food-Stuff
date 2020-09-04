0a053d4777a454d33767d63231f4f0d3de98f908
TJ-Feledy <teej387@yahoo.com>
2020-09-04 17:54:48 -0500

    Added all css files to next

--- food-stuff/pages/_app.js
+++ food-stuff/pages/_app.js
@@ -1,4 +1,11 @@
+import '../src/App.css'
+import '../src/index.css'
 import '../src/Components/Logo/Logo.css'
+import '../src/Components/LandingPage/LandingPage.css'
+import '../src/Components/Login/Login.css'
+import '../src/Components/Nav/Nav.css'
+import '../src/Components/Register/Register.css'
+
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
