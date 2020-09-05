1ab97f6dd0f0fd229b7db02b56ea7fd4bee7eb9b
TJ-Feledy <teej387@yahoo.com>
2020-09-05 13:58:51 -0500

    moved title and fixed Document

--- food-stuff/pages/_app.js
+++ food-stuff/pages/_app.js
@@ -2,12 +2,14 @@ import '../scss/style.scss'
 
 import Nav from '../src/Components/Nav/Nav.js'
 import { withRouter } from 'next/router'
+import Head from 'next/head'
 
 
 function MyApp({ Component, pageProps, router }) {
     console.log(router.pathname, 'path!!!')
     return (
       <div className='App'>
+        <Head><title>Food-Stuff</title></Head>
         {router.pathname !== '/' ? <Nav/> : null}
         <Component {...pageProps} />
       </div>
--- food-stuff/pages/_document.js
+++ food-stuff/pages/_document.js
@@ -1,17 +1,15 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
     render() {
         return (
-            <html>
-                <Head>
-                    <title>Food-Stuff</title>
-                </Head>
+            <Html>
+                <Head />
                 <body>
                     <Main />
                     <NextScript />
                 </body>
-            </html>
+            </Html>
         )
     }
 }
