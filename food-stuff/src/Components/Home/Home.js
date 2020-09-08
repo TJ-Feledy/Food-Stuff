58a939af97ca8741f43af40df2f108d0966fe4f7
TJ-Feledy <teej387@yahoo.com>
2020-09-07 20:58:32 -0500

    Created autocomplete search function

--- food-stuff/src/Components/Home/Home.js
+++ food-stuff/src/Components/Home/Home.js
@@ -1,12 +1,43 @@
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 function Home(props) {
+    const [username, setUsername] = useState('guest')
+    const [searchQuery, setSearchQuery] = useState('')
+    const [queryResults, setQueryResults] = useState([])
 
+    // GET REQUEST FOR RECIPE SEARCH
+    const handleSearch = (evt) => {
+        setSearchQuery(evt.target.value)
+        axios({
+            "method":"GET",
+            "url":"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/autocomplete",
+            "headers":{
+            "content-type":"application/octet-stream",
+            "x-rapidapi-host":"spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "x-rapidapi-key":"128a3347eamsh5bbc03809bc5489p19fd87jsn79b91bffb34e",
+            "useQueryString":true
+            },"params":{
+            "number":"10",
+            "query":searchQuery
+            }
+            })
+            .then((response)=>{
+                console.log(response.data)
+                setQueryResults(response.data)
+            })
+            .catch((error)=>{
+              console.log(error)
+            })
+    }
 
     return (
         <div className='Home'>
             <h1>HOME</h1>
+            <form className='search'>
+                <input className='searchBar' type='text' placeholder='Search for a recipe' onChange={handleSearch} />
+            </form>
         </div>
     )
 }
