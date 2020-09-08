58b9584d2d32f965dccbaf8bcc886d9c43fabaeb
TJ-Feledy <teej387@yahoo.com>
2020-09-08 00:07:22 -0500

    structured and styled recipe results

--- food-stuff/src/Components/RecipeSmall/RecipeSmall.js
+++ food-stuff/src/Components/RecipeSmall/RecipeSmall.js
@@ -5,7 +5,10 @@ function RecipeSmall(props) {
     console.log(props)
     return (
         <div className='RecipeSmall'>
-            <h3 className='resultTitle'>{props.result.title}</h3>
+            <h4 className='resultTitle'>{props.result.title}</h4>
+            <div className='resultImgContainer'>
+                <img className='resultImg' src={`https://spoonacular.com/recipeImages/${props.result.id}-312x231.jpg`} alt={'food'} />
+            </div>
         </div>
     )
 }
