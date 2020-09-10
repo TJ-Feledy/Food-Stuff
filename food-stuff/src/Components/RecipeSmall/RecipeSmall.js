d9e45cb10cb90736b3ef7b82f67b3290e5c7e94a
TJ-Feledy <teej387@yahoo.com>
2020-09-09 19:43:26 -0500

    added makeTime and servings

--- food-stuff/src/Components/RecipeSmall/RecipeSmall.js
+++ food-stuff/src/Components/RecipeSmall/RecipeSmall.js
@@ -3,14 +3,16 @@
 
 function RecipeSmall(props) {
     console.log(props)
+    const { id, image, readyInMinutes, servings, title } = props.result
     return (
         <div className='RecipeSmall'>
-            <h4 className='resultTitle'>{props.result.title}</h4>
-            <div className='resultImgContainer'>
-                <img className='resultImg' src={`https://spoonacular.com/recipeImages/${props.result.id}-312x231.jpg`} alt={'food'} />
+            <h4 className='resultTitleSmall'>{title}</h4>
+            <div className='resultImgContainerSmall'>
+                <img className='resultImgSmall' src={`https://spoonacular.com/recipeImages/${id}-312x231.jpg`} alt={'food'} />
             </div>
-            <div className='servingTimeContainer'>
-                
+            <div className='servingTimeContainerSmall'>
+                <p className='makeTimeSmall'>Ready to eat in: {readyInMinutes} minutes</p>
+                <span className='servingsSmall'>Makes {servings} servings.</span>
             </div>
         </div>
     )
