9efcedfe3582f4c983fa41f6edd90809631fff93
TJ-Feledy <teej387@yahoo.com>
2020-09-20 13:19:34 -0500

    created menuToggle function

--- food-stuff/src/Components/Nav/Menu.js
+++ food-stuff/src/Components/Nav/Menu.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 
-const Menu = (props) => {
-    return (
-        <div className='Menu'>
 
+const ClickedMenu = (props) => {
+    const [ userData, setUserData ] = useState({})
+
+    return (
+        <div className='ClickedMenu'>
+
+        </div>
+    )
+}
+
+const Menu = (props) => {
+    const [ toggleMenu, setToggleMenu ] = useState(false)
+
+    const menuClick = (evt) => {
+        console.log('clicked', toggleMenu)
+        setToggleMenu(!toggleMenu)
+    }
+
+    return (
+        <div className='Menu' onClick={menuClick}>
+            <div>menu</div>
+            {
+                toggleMenu ? <div>hi</div> : null
+            }
         </div>
     )
 }
